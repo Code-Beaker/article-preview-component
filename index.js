9212e81b8fbd00791341401375e0e554e79bf193
Tharun Raj <tharun.2.linux@gmail.com>
2024-04-01 08:59:58 +0530

    changed some positioning and added close button

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const shareButton = document.querySelector("#shareButton");
 const popup = document.querySelector("#popup");
+const closeButton = document.querySelector("#closeBtn");
 
 shareButton.addEventListener("click", () => {
   popup.classList.toggle("popup-show");
+});
+
+closeButton.addEventListener("click", () => {
+  popup.classList.add("popup");
 });
